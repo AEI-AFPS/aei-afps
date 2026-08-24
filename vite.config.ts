3683e6afc29df0a454e8ebed5808877abac40cb4
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-08-24 18:09:38 +0530

    made changes according to the vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aei-fireguard-systems/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
